08eddf045c61ed2a713778bb2fe2b03c7f13093e
Aa-nn-aA <151867154+Aa-nn-aA@users.noreply.github.com>
2025-03-27 15:49:36 +0530

    Add files via upload

---
+++ dashboard.js.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const startBtn = document.getElementById("startBtn");
+
+    startBtn.addEventListener("click", function() {
+        alert("Welcome to Soul Heaven! Your journey begins now. ✨");
+    });
+});
